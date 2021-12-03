1a8d87fc7293459f4475bb81b63390d577796430
Ewan <zyc2339@gmail.com>
2021-12-03 16:44:34 +1300

    git set up

---
+++ src/components/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer(options) {
+  return <footer className="footer">this is footer</footer>;
+}
---
+++ src/components/Home.js
@@ -0,0 +1,3 @@
+export default function Header(options) {
+  return <div className="main">this is body</div>;
+}
