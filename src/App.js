20a13102f381062a31c034f84f335049e0a9720f
Ewan <zyc2339@gmail.com>
2021-12-08 00:51:36 +1300

    add autocomplete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/HomePage/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Mainfilter from "./components/FilterPage/mainfilter";
+import ThemePractice from "./components/HomePage/theme";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/find" element={<Mainfilter />} />
+          <Route exact path="/abc" element={<ThemePractice />} />
         </Routes>
       </Router>
     </div>
